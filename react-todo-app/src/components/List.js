1ae6426ad73a6bd5ea6d9c07e397d8c12e05aac4
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-12-31 01:10:48 +0900

    feat: todo list edit 기능 추가

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,41 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = React.memo(({
-    id, title, completed, todoData, setTodoData, provided, snapshot, handleClick
-  }) => {
+  id, title, completed, todoData, setTodoData, provided, snapshot, handleClick
+}) => {
   
-    const handleCompleteChange = (id) => {
-      const newTodoData = todoData.map((data) => {
-        if(data.id === id) {
-          data.completed = !completed
-        }
-        return data
-      })
-      setTodoData(newTodoData)
-    }
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedTitle, setEditedTitle] = useState(title)
 
-  return (
-    <div 
-      key={id} 
-      {...provided.draggableProps} 
-      ref={provided.innerRef} 
-      {...provided.dragHandleProps}
-      className={
-        `${snapshot.isDragging ? "bg-gray-50" : ""} flex items-center justify-between w-full px-4 py-1 my-2`
-      } >
-      <div className='items-center'>
-        <input 
-          type="checkbox"
-          onChange={() => handleCompleteChange(id)}
-          defaultChecked={completed}
-        />
-        <span className={completed ? "line-through" : undefined}>{" "}{title}</span>
+  const handleCompleteChange = (id) => {
+    const newTodoData = todoData.map((data) => {
+      if(data.id === id) {
+        data.completed = !completed
+      }
+      return data
+    })
+    setTodoData(newTodoData)
+  }
+
+  const handleEditChange = (e) => {
+    setEditedTitle(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newTodoData = todoData.map((data) => {
+      if(data.id === id) {
+        data.title = editedTitle
+      }
+      return data
+    })
+    setTodoData(newTodoData)
+    setIsEditing(false)
+  }
+
+  if(isEditing) {
+    return(
+      <div 
+        className={ 
+          'flex items-center justify-between w-full px-4 py-1 my-2'
+        } >
+        <div className='items-center'>
+          <form onSubmit={handleSubmit}>
+            <input 
+              value={editedTitle}
+              className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+              onChange={handleEditChange}
+              autoFocus
+            />
+          </form>
+        </div>
+        <div className='items-center'>
+          <button 
+            className='px-4 py-2 float-right'
+            onClick={() => setEditedTitle(false)}
+          >
+            x
+          </button>
+          <button onClick={handleSubmit}
+            className='px-4 py-2 float-right'
+            type='submit'
+          >
+            save
+          </button>
+        </div>
       </div>
-      <div className='items-center'>
-        <button onClick={() => handleClick(id)}>x</button>
+    )
+  } else {
+    return (
+      <div 
+        key={id} 
+        {...provided.draggableProps} 
+        ref={provided.innerRef} 
+        {...provided.dragHandleProps}
+        className={
+          `${snapshot.isDragging ? "bg-gray-50" : ""} flex items-center justify-between w-full px-4 py-1 my-2`
+        } >
+        <div className='items-center'>
+          <input 
+            type="checkbox"
+            onChange={() => handleCompleteChange(id)}
+            defaultChecked={completed}
+          />
+          <span className={completed ? "line-through" : undefined}>{" "}{title}</span>
+        </div>
+        <div className='items-center'>
+          <button 
+            className='px-4 py-2 float-right'
+            onClick={() => handleClick(id)}
+          >
+            x
+          </button>
+          <button 
+            className='px-4 py-2 float-right'
+            onClick={() => setIsEditing(true)}
+          >
+            edit
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 })
 
 export default List
