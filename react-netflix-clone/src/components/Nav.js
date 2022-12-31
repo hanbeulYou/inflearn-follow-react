b9a22eaf1f63352ec6ac279fd179739ba1e14c53
hanbeulYou <hanbeul.you@gmail.com>
2022-12-31 19:16:08 +0900

    feat: navigation bar component 생성

---
+++ react-netflix-clone/src/components/Nav.js
@@ -0,0 +1,36 @@
+import { React, useState, useEffect } from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+  
+  const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 50) {
+        handleShow(true)
+      } else {
+        handleShow(false)
+      }
+    })
+    return () => {
+      window.removeEventListener("scroll", () => {})
+    }
+  }, [])
+
+  return (
+    <nav className={`nav ${show && 'nav__black'}`}>
+      <img
+        alt='Netflix Logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/440px-Netflix_2015_logo.svg.png'
+        className='nav__logo'
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'
+        className='nav__avatar'
+      />
+    </nav>
+  )
+}
