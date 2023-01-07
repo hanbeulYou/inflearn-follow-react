963a7c27d659b49f983934624788af930c92672d
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2023-01-08 00:24:09 +0900

    feat: minus-button 클릭 시 count값 감소

--- react-button-app/src/App.js
+++ react-button-app/src/App.js
@@ -8,7 +8,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <button data-testid="minus-button">-</button>
+        <button 
+          data-testid="minus-button"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          -
+        </button>
         <button 
           data-testid="plus-button"
           onClick={() => setCount((count) => count + 1)}
